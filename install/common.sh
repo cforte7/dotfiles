dacccea69ba96cd2871bbbe2e8e8bedd65eda430
Christopher Forte <cforte7@gmail.com>
2025-11-30 21:17:17 -0600

    execute brewfile in common

--- install/common.sh
+++ install/common.sh
@@ -3,3 +3,12 @@ if ! command -v brew >/dev/null; then
   echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+BREWFILE="$HOME/.dotfiles/brew/Brewfile"
+
+if [ -f "$BREWFILE" ]; then
+  echo "Running Brewfile..."
+  brew bundle --file="$BREWFILE"
+fi
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
