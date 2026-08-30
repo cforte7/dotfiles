4f4ffafc09d24dc1c5f349601d6a66f1be99f25d
Christopher Forte <cforte7@gmail.com>
2026-08-29 20:55:48 -0500

    more brew file into mac install

--- install/mac.sh
+++ install/mac.sh
@@ -3,3 +3,11 @@ if ! command -v brew >/dev/null; then
   echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+BREWFILE="$HOME/dotfiles/brew/Brewfile"
+
+if [ -f "$BREWFILE" ]; then
+  printf "Running Brewfile..."
+  brew bundle --file="$BREWFILE"
+fi
+printf "Brewfile complete\n\n"
